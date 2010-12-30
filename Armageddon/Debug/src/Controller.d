1d19139e5399321f9d50faa5463b64c21d98da5b
shadowpt <nuno_costa@live.com>
2010-12-30 17:53:47 +0000

    finally Armageddon with Eclipse

---
+++ Armageddon/Debug/src/Controller.d
@@ -0,0 +1,126 @@
+src/Controller.d src/Controller.o: ../src/Controller.cpp \
+ ../src/Controller.h ../src/Hero.h \
+ /home/pinto/workspace/cglib/src/cg/cg.h \
+ /home/pinto/workspace/cglib/src/cg/Application.h \
+ /home/pinto/workspace/cglib/src/cg/DebugFile.h \
+ /home/pinto/workspace/cglib/src/cg/DebugNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/IDebugListener.h \
+ /home/pinto/workspace/cglib/src/cg/Notifier.h \
+ /home/pinto/workspace/cglib/src/cg/Entity.h \
+ /home/pinto/workspace/cglib/src/cg/State.h \
+ /home/pinto/workspace/cglib/src/cg/LockableOrderedMap.h \
+ /home/pinto/workspace/cglib/src/cg/CommandQueue.h \
+ /home/pinto/workspace/cglib/src/cg/Singleton.h \
+ /home/pinto/workspace/cglib/src/cg/DrawNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/IDrawListener.h \
+ /home/pinto/workspace/cglib/src/cg/DrawOverlayNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/IDrawOverlayListener.h \
+ /home/pinto/workspace/cglib/src/cg/KeyBuffer.h \
+ /home/pinto/workspace/cglib/src/cg/KeyboardEventNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/IKeyboardEventListener.h \
+ /home/pinto/workspace/cglib/src/cg/MouseEventNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/IMouseEventListener.h \
+ /home/pinto/workspace/cglib/src/cg/Properties.h \
+ /home/pinto/workspace/cglib/src/cg/Vector.h \
+ /home/pinto/workspace/cglib/src/cg/Registry.h \
+ /home/pinto/workspace/cglib/src/cg/GroupDebug.h \
+ /home/pinto/workspace/cglib/src/cg/Group.h \
+ /home/pinto/workspace/cglib/src/cg/GroupDraw.h \
+ /home/pinto/workspace/cglib/src/cg/GroupDrawOverlay.h \
+ /home/pinto/workspace/cglib/src/cg/GroupKeyboardEvent.h \
+ /home/pinto/workspace/cglib/src/cg/GroupMouseEvent.h \
+ /home/pinto/workspace/cglib/src/cg/GroupReshapeEvent.h \
+ /home/pinto/workspace/cglib/src/cg/IReshapeEventListener.h \
+ /home/pinto/workspace/cglib/src/cg/GroupUpdate.h \
+ /home/pinto/workspace/cglib/src/cg/IUpdateListener.h \
+ /home/pinto/workspace/cglib/src/cg/ReshapeEventNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/UpdateNotifier.h \
+ /home/pinto/workspace/cglib/src/cg/Util.h \
+ /home/pinto/workspace/cglib/src/cg/Manager.h \
+ /home/pinto/workspace/cglib/src/cg/Quaternion.h \
+ /home/pinto/workspace/cglib/src/cg/Test.h \
+ /home/pinto/workspace/cglib/src/cg/TestSuite.h
+
+../src/Controller.h:
+
+../src/Hero.h:
+
+/home/pinto/workspace/cglib/src/cg/cg.h:
+
+/home/pinto/workspace/cglib/src/cg/Application.h:
+
+/home/pinto/workspace/cglib/src/cg/DebugFile.h:
+
+/home/pinto/workspace/cglib/src/cg/DebugNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/IDebugListener.h:
+
+/home/pinto/workspace/cglib/src/cg/Notifier.h:
+
+/home/pinto/workspace/cglib/src/cg/Entity.h:
+
+/home/pinto/workspace/cglib/src/cg/State.h:
+
+/home/pinto/workspace/cglib/src/cg/LockableOrderedMap.h:
+
+/home/pinto/workspace/cglib/src/cg/CommandQueue.h:
+
+/home/pinto/workspace/cglib/src/cg/Singleton.h:
+
+/home/pinto/workspace/cglib/src/cg/DrawNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/IDrawListener.h:
+
+/home/pinto/workspace/cglib/src/cg/DrawOverlayNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/IDrawOverlayListener.h:
+
+/home/pinto/workspace/cglib/src/cg/KeyBuffer.h:
+
+/home/pinto/workspace/cglib/src/cg/KeyboardEventNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/IKeyboardEventListener.h:
+
+/home/pinto/workspace/cglib/src/cg/MouseEventNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/IMouseEventListener.h:
+
+/home/pinto/workspace/cglib/src/cg/Properties.h:
+
+/home/pinto/workspace/cglib/src/cg/Vector.h:
+
+/home/pinto/workspace/cglib/src/cg/Registry.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupDebug.h:
+
+/home/pinto/workspace/cglib/src/cg/Group.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupDraw.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupDrawOverlay.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupKeyboardEvent.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupMouseEvent.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupReshapeEvent.h:
+
+/home/pinto/workspace/cglib/src/cg/IReshapeEventListener.h:
+
+/home/pinto/workspace/cglib/src/cg/GroupUpdate.h:
+
+/home/pinto/workspace/cglib/src/cg/IUpdateListener.h:
+
+/home/pinto/workspace/cglib/src/cg/ReshapeEventNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/UpdateNotifier.h:
+
+/home/pinto/workspace/cglib/src/cg/Util.h:
+
+/home/pinto/workspace/cglib/src/cg/Manager.h:
+
+/home/pinto/workspace/cglib/src/cg/Quaternion.h:
+
+/home/pinto/workspace/cglib/src/cg/Test.h:
+
+/home/pinto/workspace/cglib/src/cg/TestSuite.h:
